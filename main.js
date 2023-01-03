a501850a12e7afe33c034d379c9c8971590e35a4
riteshreg <riteshregmi85@gmail.com>
2023-01-03 15:26:57 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+
+document.querySelector('#app').innerHTML = `
+Thanks gopal for help</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
