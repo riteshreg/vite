3faf4a9fcbe0b378401714684fb96d4779e399e8
riteshreg <riteshregmi85@gmail.com>
2023-01-03 15:48:47 +0545

    h2 added

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import javascriptLogo from './javascript.svg'
 
 document.querySelector('#app').innerHTML = `
 <h1>Thanks gopal for help</h1>
-<p>Mugi Gopal ko Ban</p>
+<h2>Testing Vite with js</h2>
+<p>Gopal Is the Hybrid of Nepali Boko</p>
   </div>
 `
 
